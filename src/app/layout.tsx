c81db805ea968ffcd8dbc18a44528caebda36555
AreebaSadiq23 <areebasadiq41@gmail.com>
2024-12-30 20:25:25 +0500

    update layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body className={geistSans.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
